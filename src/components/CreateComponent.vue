8bf8a8e6f3960f088eacf5a568109830112a9bd0
Abtin <Abtin-Makaveli@users.noreply.github.com>
2020-02-10 10:59:37 -0500

    test

---
+++ src/components/CreateComponent.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <h1>Create A Contact</h1>
+    <form @submit.prevent="addPost">
+      <div class="row">
+        <div class="col-md-6">
+          <div class="form-group">
+            <label>Contact Name:</label>
+            <input type="text" class="form-control" v-model="post.title">
+          </div>
+        </div>
+        </div>
+        <div class="row">
+        <div class="col-md-6">
+          <div class="form-group">
+            <label>Contact Number:</label>
+            <input type="text" class="form-control" v-model="post.number">
+          </div>
+        </div>
+        </div>
+        <div class="row">
+          <div class="col-md-6">
+            <div class="form-group">
+              <label>Contact Address:</label>
+              <textarea class="form-control" v-model="post.body" rows="5"></textarea>
+            </div>
+          </div>
+        </div><br />
+        <div class="form-group" id='app'>
+          <b-button @click="addPost()">Click Me</b-button>
+          <!--<button class="btn btn-primary">Create</button>-->
+        </div>
+    </form>
+  </div>
+</template>
+
+<script>
+    export default {
+        data(){
+        return {
+          post:{}
+        }
+    },
+    methods: {
+      addPost(){
+        let uri = 'http://localhost:4000/posts/add';
+    this.axios.post(uri, this.post).then(() => {
+       this.$router.push({name: 'posts'});
+    });
+      }
+    }
+  }
+</script>
