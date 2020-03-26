b1d5c595b702815b46db34ba40b4d8e47515da5e
Abtin <abtin.abbaspour@gmail.com>
2020-03-25 20:11:49 -0400

    Css work

--- src/App.vue
+++ src/App.vue
@@ -1,46 +1,31 @@
 <template>
-<div>
+  <div>
     <b-navbar class="is-second navbar">
-        <template slot="brand">
-            <b-navbar-item tag="router-link" :to="{ path: '/' }">
-                <img
-                    src="https://i.ibb.co/B6fwcVd/gringotts.png"
-                    height="3rem"
-                >
-            </b-navbar-item>
-        </template>
-        <template slot="start">
-            <b-navbar-item tag="router-link" :to="{ path: '/' }">
-                Home
-            </b-navbar-item>
-            <b-navbar-item tag="router-link" :to="{ path: '/create' }">
-                Create
-            </b-navbar-item>
-            <b-navbar-item tag="router-link" :to="{ path: '/posts' }">
-                Posts
-            </b-navbar-item>
-            <b-navbar-dropdown label="Info">
-                <b-navbar-item href="#">
-                    About
-                </b-navbar-item>
-                <b-navbar-item href="#">
-                    Contact
-                </b-navbar-item>
-            </b-navbar-dropdown>
-        </template>
+      <template slot="brand">
+        <b-navbar-item tag="router-link" :to="{ path: '/' }">
+          <img src="https://i.ibb.co/B6fwcVd/gringotts.png" height="3rem" />
+        </b-navbar-item>
+      </template>
+      <template slot="start">
+        <b-navbar-item tag="router-link" :to="{ path: '/' }">Home</b-navbar-item>
+        <b-navbar-item tag="router-link" :to="{ path: '/create' }">Create</b-navbar-item>
+        <b-navbar-item tag="router-link" :to="{ path: '/posts' }">Posts</b-navbar-item>
+        <b-navbar-dropdown label="Info" >
+          <b-navbar-item href="#" class="is-first navbar">About</b-navbar-item>
+          <b-navbar-item href="#" class="is-first navbar">Contact</b-navbar-item>
+        </b-navbar-dropdown>
+      </template>
 
-        <template slot="end">
-            <b-navbar-item tag="div">
-                <div class="buttons">
-                    <a class="button is-primary">
-                        <strong>Sign up</strong>
-                    </a>
-                    <a class="dark button is-light">
-                        Log in
-                    </a>
-                </div>
-            </b-navbar-item>
-        </template>
+      <template slot="end">
+        <b-navbar-item tag="div">
+          <div class="buttons">
+            <a class="button is-primary">
+              <strong>Sign up</strong>
+            </a>
+            <a class="dark button is-light">Log in</a>
+          </div>
+        </b-navbar-item>
+      </template>
     </b-navbar>
     <br />
     <transition name="fade">
@@ -56,7 +41,7 @@
 // Set your colors
 $primary: #8c67ef;
 $primary-invert: findColorInvert($primary);
-$twitter: #4099FF;
+$twitter: #4099ff;
 $twitter-invert: findColorInvert($twitter);
 
 $first: #9a841c;
@@ -66,18 +51,18 @@ $second-invert: findColorInvert($second);
 
 // Setup $colors to use as bulma classes (e.g. 'is-twitter')
 $colors: (
-    "white": ($white, $black),
-    "black": ($black, $white),
-    "light": ($light, $light-invert),
-    "dark": ($dark, $dark-invert),
-    "primary": ($primary, $primary-invert),
-    "info": ($info, $info-invert),
-    "success": ($success, $success-invert),
-    "warning": ($warning, $warning-invert),
-    "danger": ($danger, $danger-invert),
-    "twitter": ($twitter, $twitter-invert),
-    "first": ($first, $first-invert),
-    "second": ($second, $second-invert)
+  "white": ($white, $black),
+  "black": ($black, $white),
+  "light": ($light, $light-invert),
+  "dark": ($dark, $dark-invert),
+  "primary": ($primary, $primary-invert),
+  "info": ($info, $info-invert),
+  "success": ($success, $success-invert),
+  "warning": ($warning, $warning-invert),
+  "danger": ($danger, $danger-invert),
+  "twitter": ($twitter, $twitter-invert),
+  "first": ($first, $first-invert),
+  "second": ($second, $second-invert)
 );
 
 // Links
@@ -85,16 +70,25 @@ $link: $primary;
 $link-invert: $primary-invert;
 $link-focus-border: $primary;
 
-
+// Navbar height
 $navbar-item-img-max-height: 3rem !important;
 
 // Import Bulma and Buefy styles
 @import "~bulma";
 @import "~buefy/src/scss/buefy";
+
+// Classes
+.navbar {
+}
+
+body {
+  background-color: #f7e79c;
+  background-size: cover;
+  height: 941px;
+}
+
 </style>
 
 <script>
-
-    export default{
-    }
+export default {};
 </script>
