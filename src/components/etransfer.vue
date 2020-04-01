f5c1175f709ce0de621f2d362fcb13e62e4cbf20
batjonsinaj <43096541+batjonsinaj@users.noreply.github.com>
2020-04-01 11:39:43 -0400

    e transfer v1

--- src/components/etransfer.vue
+++ src/components/etransfer.vue
@@ -1,22 +1,42 @@
 <template>
   <div>
-    <h1>ETransfer Money</h1>
-    <form>
-      <div class="row">
-        <b-field label = "ETransfer Amount:">
+  <p class="title is-0.5" text-align = "middle">E-Transfer Money</p>
+  <p class="subtitle is-5">.Gringotts sends a notice to the email address you provide within 5 minutes.</p>
+  <p class="subtitle is-5">.Fees apply to sending an interac E-transfer transaction.</p>
+  <p class="subtitle is-5">.The transfer has to be more than 1$.</p>
+  <p class="subtitle is-5">.E-transfer transactions expire after 30 days.</p>
+  <p class="subtitle is-5">.If you want an interac E-transfer from one of your Gringott accounts, use another email.</p>
+  <p class="subtitle is-5">.Choose a forex currency that you wish to transfer. NOTE: Transaction may change depending on that currency you choose, this will appear on contacts account.</p>
+      <div class="column">
+        <b-field label = "Send Money To:">
+        <b-input v-model = "post.eTransferTo" label = "E-Transfer to:" placeholder="Email Address" minLength="2" icon="account">          </b-input>
+         </b-select>
+            </b-field>
+        </div>
+        <form>
+      <div class="column">
+        <b-field label = "ETransfer Currency:">
             <b-select placeholder="Currency">
                 <option>$CAD</option>
+                <option>$USD</option>
+                <option>$EURO</option>
+                <option>$AUD</option>
+                <option>$CHF</option>
+                <option>$GBP</option>
             </b-select>
-            <b-input v-model = "post.amount" type="number" step = "0.01" placeholder="0.00" min = "0.01"></b-input>
-            <b-input v-model = "post.eTransferTo" label = "E-Transfer to:" placeholder="FirstName LastName" minLength="2" icon="account">
-            </b-input>
-            <p class="control">
+            </b-field>
+        </div>
+        <br/>
+    </form>
+    <b-field label = "Amount:">
+    <b-input v-model = "post.amount" type="number" placeholder="$" min = "1.00"></b-input>
+    <p class="control">
                 <button class="button is-success" @click.prevent="eTransfer()">ETransfer</button>
             </p>
-        </b-field>
-        </div>
-        <br />
-    </form>
+    </b-select>
+   </b-field>
+     <p class="subtitle is-5"></p>
+     <p class="subtitle is-5">When you're ready, click to send the transaction.</p>
   </div>
 </template>
 
@@ -30,28 +50,3 @@
     },
     created() {
       let uri = 'http://localhost:4000/posts';
-      this.axios.get(uri).then(response => {
-        this.posts = response.data;
-      });
-    },
-    methods: {
-      eTransfer(){
-        this.post.amount = parseFloat(this.post.amount).toFixed(2);
-        if(this.posts.length === 0){
-            this.$buefy.snackbar.open(`Action failed - you have no balance in your account to withdraw.`);
-           return;
-        } else if(parseFloat(this.post.amount) > parseFloat(this.posts[this.posts.length-1].balanceAfter)){
-            this.$buefy.snackbar.open(`Action failed - you do not have $` + parseFloat(this.post.amount).toFixed(2) + ` balance in your account to withdraw.`);
-           return;
-        }else{
-          this.post.balanceAfter = parseFloat(this.posts[this.posts.length-1].balanceAfter) - parseFloat(this.post.amount).toFixed(2);
-          let uri = 'http://localhost:4000/posts/add';
-          this.axios.post(uri, this.post).then(() => {
-          this.$router.push({name: 'homePage'});
-          console.log("successful transaction");
-          });
-        }
-      }
-    }
-  }
-</script>
