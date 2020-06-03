f8519c8cda3a16116af4569ea262f03dd224f882
Ben Merbaum <46571024+bmerbaum@users.noreply.github.com>
2020-06-03 11:01:36 -0400

    Buefy added to login1.vue

--- src/components/login1.vue
+++ src/components/login1.vue
@@ -3,23 +3,50 @@
 <div>
     <div class = "columns">
         <div class = "column">
-        <p class="title is-0.5" text-align = "middle">Log In</p>
+            <div class = "myBox" style="background-color: #9a841c">
+                <p class="title is-2" text-align = "middle">Log In</p><br>
 
-        <b-field label="Username">
-            <b-input maxlength="10"></b-input>
-        </b-field>
+                <b-field label="Username">
+                    <b-input maxlength="10"></b-input>
+                </b-field>
 
-        <b-field label="Email">
-            <b-input type="email"
-                maxlength="20">
-            </b-input>
-        </b-field>
+                <b-field label="Email">
+                    <b-input type="email"
+                        maxlength="20">
+                    </b-input>
+                </b-field>
 
-        <b-field label="Password">
-            <b-input type="password" maxlength="16"></b-input>
-        </b-field>
+                <b-field label="Password">
+                    <b-input type="password" maxlength="16"></b-input>
+                </b-field>
 
-        <button>Log In</button>
+                <b-button class = "myButton" type="is-second" size = "is-large">Log In</b-button>
+            </div>
+        </div>
+        <div id = "line"></div>
+        <div class = "column"> 
+            <div class = "myBox" style="background-color: #9a841c">
+                <p class="title is-2" text-align = "middle">Register</p><br>
+
+                <b-field label="Username">
+                    <b-input maxlength="10"></b-input>
+                </b-field>
+
+                <b-field label="Email">
+                    <b-input type="email"
+                        maxlength="20">
+                    </b-input>
+                </b-field>
+
+                <b-field label="Password">
+                    <b-input type="password" maxlength="16"></b-input>
+                </b-field>
+                <b-field label="Confirm Password">
+                    <b-input type="password" maxlength="16"></b-input>
+                </b-field>
+
+                <b-button class = "myButton" type="is-second" size = "is-large">Register</b-button>
+            </div>
         </div>
     </div>
 </div>
@@ -46,9 +73,19 @@ export default {
 
 <style>
 
-.box{
-  width: 100px;
+#line{
+    margin:100px 0px;
+    width:4px;
+    height:auto;
+    background-color: #482e06;
 }
+
+.myBox{
+  margin: 10px 40px;
+  padding: 60px;
+  border-radius: 30px;
+}
+
 #image{
   width: 500px;
    margin-left: 70px;
@@ -58,6 +95,10 @@ export default {
    margin-right:60px;
    float:right;
    width: 700px;
+}
+
+.myButton{
+    margin:10px 0px -20px 0px;
 }
 
 </style>
